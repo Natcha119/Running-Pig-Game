8e9b4563d7b45a457735dbc0a820b8c87f7cd2d7
Natcha119 <natcha119@hotmail.com>
2014-04-11 11:48:05 +0700

    modify variable name

--- src/Background.js
+++ src/Background.js
@@ -2,26 +2,30 @@ var Background = cc.Node.extend({
 		ctor: function() {
 			this._super();
 			
-			this.bg1 = cc.Sprite.create(' images/floor3.jpg ');
-			this.bg1.setPosition( 400,400 );
-			this.addChild( this.bg1 );
+			this.background1 = cc.Sprite.create(' images/floor3.jpg ');
+			this.background1.setPosition( 400, 400 );
+			this.addChild( this.background1 );
 			
-			this.bg2 = cc.Sprite.create(' images/floor3.jpg ');
-			this.bg2.setPosition(400,1200);
-			this.addChild( this.bg2 );
+			this.background2 = cc.Sprite.create(' images/floor3.jpg ');
+			this.background2.setPosition( 400, 1200 );
+			this.addChild( this.background2 );
 		},
 		
 		update: function( dt ) {
  
-			if ( this.bg1.getPositionY() < -400 ) {
-				this.bg1.setPosition( new cc.Point( 400, this.bg2.getPositionY()+800 ) );
+			if ( this.background1.getPositionY() < -400 ) {
+				this.background1.setPosition( new cc.Point( 400, this.background2.getPositionY()+800 ) );
 			} 
-			else if( this.bg2.getPositionY() < -400 ) {
-				this.bg2.setPosition( new cc.Point( 400, this.bg1.getPositionY()+800 ) );
+			else if( this.background2.getPositionY() < -400 ) {
+				this.background2.setPosition( new cc.Point( 400, this.background1.getPositionY()+800 ) );
 			}
 			
-			this.bg1.setPosition( new cc.Point( 400, this.bg1.getPositionY() - 4 ) );
-			this.bg2.setPosition( new cc.Point( 400, this.bg2.getPositionY() - 4 ) );
-		}
+			this.slideBackground();
+		},
+		
+		slideBackground : function(){
+			this.background1.setPosition( new cc.Point( 400, this.background1.getPositionY() - 4 ) );
+			this.background2.setPosition( new cc.Point( 400, this.background2.getPositionY() - 4 ) );
+		},
 	
 });
